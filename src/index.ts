fedae25bf52f7d4c9e5cf0084b0fa299f713e880
Dan O'Neill <dev@jdoneill.com>
2025-06-24 15:26:48 -0700

    add new lib entry points

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
-export { generateUISchema } from './generateUISchema';
-export { validateJSONSchema } from './validateJsonSchema';
+// Default exports (backward compatible - uses v1)
+export { generateUISchema, validateJSONSchema } from './v1/index';
+
+// Versioned exports                                                                                              │
+export * as v1 from './v1/index';
