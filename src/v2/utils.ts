1770247608d2a01714df8959c9aa406f49f6cf63
Dan O'Neill <dev@jdoneill.com>
2026-09-14 13:41:48 -0700

    remove unused fallback

--- src/v2/utils.ts
+++ src/v2/utils.ts
@@ -160,9 +160,7 @@ export const createControl = (
         if (property.uniqueItems) {
           control.options!.uniqueItems = true;
         }
-        const itemKey =
-          property.items?.required?.[0] ??
-          Object.keys(property.items?.properties ?? {})[0];
+        const itemKey = property.items?.required?.[0];
         if (itemKey) {
           control.options!.itemKey = itemKey;
         }
